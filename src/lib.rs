b961927b0eb67afb0d78ef229e74de75a3a3e107
DiamondDDF <ddfalerts@gmail.com>
2022-08-29 01:24:47 -0500

    re-exported more stuff from tracing crate

--- src/lib.rs
+++ src/lib.rs
@@ -50,7 +50,7 @@ pub mod logger;
 pub mod custom_layer;
 pub mod prelude{
     pub use std::{path::PathBuf};
-    pub use tracing::{debug_span, instrument, info, warn, error, info_span, Subscriber};
+    pub use tracing::{debug_span, instrument, info, warn, error, trace, info_span, error_span, warn_span, Subscriber};
     pub use file_rotate::{ContentLimit};
     pub use crate::logger::Logger;
     pub use chrono_tz::Tz;
